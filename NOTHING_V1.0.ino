561f6314b527a250af3b1cfe6f4843eeef271746
Francisco <66030077+confus-3d@users.noreply.github.com>
2020-07-18 09:57:24 +0200

    Update NOTHING_V1.0.ino

--- NOTHING_V1.0.ino
+++ NOTHING_V1.0.ino
@@ -4,15 +4,15 @@ Timer deathTimer;
 #define DEATH_SWITCH 500
 Timer switchTimer;
 
-enum blinkState {ALIVE, DEAD1, DEAD2};
+enum blinkState {ALIVE, DEAD};
 int blinkState = ALIVE;
 
 byte hue=0;
 Timer nextStep;
+byte dead=0;
 
 void setup() {
   // put your setup code here, to run once
-
 }
 
 void loop() {
@@ -21,43 +21,19 @@ void loop() {
       aliveLoop();
       aliveDisplay;
       break;
-    case DEAD1:
-      dead1Loop();
-      break;
-    case DEAD2:
-      dead2Loop();
+    case DEAD:
+      deadLoop();
       break;
   }
-
   buttonPressed();
-  buttonSingleClicked();
-  buttonDoubleClicked();
-  buttonMultiClicked();
-  buttonLongPressed();
   setValueSentOnAllFaces(blinkState);
-
 }
 
 void aliveLoop(){
-  if(buttonPressed()){
-    transitionLoop();
-  }
-  if(buttonSingleClicked()){
-    transitionLoop();
-  }
-  if(buttonDoubleClicked()){
-    transitionLoop();
-  }
-  if(buttonMultiClicked()){
-    transitionLoop();
-  }
-  if(buttonLongPressed()){
-    transitionLoop();
-  }
-  if(isAlone()){
-    blinkState = ALIVE;
-  }else {
-    transitionLoop();
+  if(buttonPressed() || !isAlone()){
+    deathTimer.set(DEATH_INTERVAL);
+    switchTimer.set(DEATH_SWITCH);
+    blinkState = DEAD;
   }
 }
 
@@ -66,44 +42,19 @@ if (nextStep.isExpired()) {
     setColor( makeColorHSB( hue , 255 , 255 ) );
     hue++;     
     nextStep.set(40);
-
   }
 }
 
-void dead1Loop(){
+void deadLoop(){
   if (deathTimer.isExpired()) {
     blinkState = ALIVE;
   }
-  setColorOnFace(RED, 0);
-  setColorOnFace(WHITE, 1);
-  setColorOnFace(RED, 2);
-  setColorOnFace(WHITE, 3);
-  setColorOnFace(RED, 4);
-  setColorOnFace(WHITE, 5);
+  setColor(RED);
+  setColorOnFace(WHITE, dead);
+  setColorOnFace(WHITE, dead+2);
+  setColorOnFace(WHITE, dead+4);
   if (switchTimer.isExpired()) {
     switchTimer.set(DEATH_SWITCH);
-    blinkState = DEAD2;
+    dead = (dead + 1) % 2;
   }
-}
-
-void dead2Loop(){
-  if (deathTimer.isExpired()) {
-    blinkState = ALIVE;
-  }
-  setColorOnFace(WHITE, 0);
-  setColorOnFace(RED, 1);
-  setColorOnFace(WHITE, 2);
-  setColorOnFace(RED, 3);
-  setColorOnFace(WHITE, 4);
-  setColorOnFace(RED, 5);
-  if (switchTimer.isExpired()) {
-    switchTimer.set(DEATH_SWITCH);
-    blinkState = DEAD1;
-  }
-}
-  
-void transitionLoop(){
-  deathTimer.set(DEATH_INTERVAL);
-  switchTimer.set(DEATH_SWITCH);
-  blinkState = DEAD1;
 }
